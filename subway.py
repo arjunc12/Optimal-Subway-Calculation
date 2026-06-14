c70395d2123283ca926943f2dfabe89433778d45
summerygu <summerguan2027@gmail.com>
2026-06-14 20:35:59 +0000

    multiple star points

--- subway.py
+++ subway.py
@@ -1,23 +1,30 @@
 # minimize cost = a * length + (1-a) * time to downtown
 import numpy as np
 # variables
-a = 0.1 # changable
+a = 1 # changable
 downtown = np.array([0, 0])
-suburb = np.array([1, 2])
-best_cost = float("inf")
-best_star = None
+suburbs = np.array([
+    [1, 2],
+    [2, 5],
+    [-1, 3],
+])
+main_length = 5
 
-for y in np.linspace(0, 2, 500):
-    star = np.array([0, y])
-    length = np.linalg.norm(suburb - star)
-    time = abs(y) + length
-    cost = a * length + (1-a) * time
-    if cost < best_cost:
-        best_cost = cost
-        best_star = star
+for suburb in suburbs:
+    best_cost = float("inf")
+    best_star = None
 
-print("Best star point:", best_star)
-print("Minimum cost", best_cost)
+    for y in np.linspace(0, main_length, main_length * 100):
+        star = np.array([0, y])
+        length = np.linalg.norm(suburb - star)
+        time = abs(y) + length
+        cost = a * length + (1-a) * time
+        if cost < best_cost:
+            best_cost = cost
+            best_star = star
+
+    print("Best star point:", best_star)
+    print("Minimum cost", best_cost)
 
 
 
